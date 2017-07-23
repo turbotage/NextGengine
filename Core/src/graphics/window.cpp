ac621c66b99f1f27e838bf307b4bdbfed161eb05
turbotage <viktor.vigrennaslund@tasetek.com>
2017-07-23 16:38:48 +0200

    started working on window

--- Core/src/graphics/window.cpp
+++ Core/src/graphics/window.cpp
@@ -1,2 +1,21 @@
 #include "window.h"
 
+void ng::graphics::Window::OnMouseEvent(eAction action, int16 x, int16 y, uint8 btn)
+{
+
+}
+
+void ng::graphics::Window::OnKeyEvent(eAction action, uint8 keycode)
+{
+
+}
+
+void ng::graphics::Window::OnTextEvent(const char * str)
+{
+
+}
+
+void ng::graphics::Window::OnResizeEvent(uint16 width, uint16 height)
+{
+
+}
