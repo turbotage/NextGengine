62feecb06a281f50fdbc00fd5d0a49fcf5fb4bfb
turbotage <viktor.vigrennaslund@tasetek.com>
2017-08-30 23:09:28 +0200

    started working on graphics pipeline

--- Core/src/Graphics/Vulkan/Pipeline/vulkan_graphics_pipeline.h
+++ Core/src/Graphics/Vulkan/Pipeline/vulkan_graphics_pipeline.h
@@ -1,15 +1,36 @@
 #pragma once
 #include "vulkan_pipeline.h"
+#include "../../window.h"
+
 
 namespace ng {
 	namespace graphics {
+
+		enum NgShaderType {
+			VERTEX_SHADER_BIT = 5,
+			TESSELLATION_SHADER_BIT = 10,
+			FRAGMENT_SHADER_BIT = 20
+		};
+
 		class VulkanGraphicsPipeline : VulkanPipeline
 		{
-		public:
-			
-			
+			VkDevice* m_Device;
 
-			void createGraphicsPipeline();
+			Window* m_Window;
+		public:
+
+			VkRenderPass renderPass;
+			VkPipelineLayout pipelineLayout;
+
+			VkShaderModule vertShaderModule = VK_NULL_HANDLE;
+			VkShaderModule tessellShaderModule = VK_NULL_HANDLE;
+			VkShaderModule fragShaderModule = VK_NULL_HANDLE;
+
+			void createRenderPass();
+			void freeRenderPass();
+
+			void createGraphicsPipeline(Window* window, std::vector<std::pair<std::string, NgShaderType>>* shaderPaths);
+			void freeGraphicsPipeline();
 
 		};
 	}
